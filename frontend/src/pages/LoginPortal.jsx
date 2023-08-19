b5c30adbab8c2450224c6391060ab63c3d14edb3
charan sai v <vcharansaiv@gmail.com>
2023-08-19 14:39:37 +0530

    featured events add in user dashboard

--- frontend/src/pages/LoginPortal.jsx
+++ frontend/src/pages/LoginPortal.jsx
@@ -44,7 +44,7 @@ export default function LoginPortal() {
     </ul>
       </div>
       <div>
-      <section id="user-login" className="bg-gray-50 dark:bg-gray-900 hidden">
+      <section id="user-login" className="bg-gray-50 dark:bg-gray-900 hidden" >
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto 1 lg:py-0">
             
             <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -94,7 +94,7 @@ export default function LoginPortal() {
                     </p>
                     <form className="space-y-4 md:space-y-6" action="#">
                         <div>
-                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                            <label   htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                             <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
                         </div>
                         <div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,30 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': [
+        'Inter var',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
+      ],
+      'serif': ['ui-serif', 'Georgia', 'Times New Roman', 'serif'],
+      'mono': ['ui-monospace', 'SFMono-Regular', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],
+      'display': ['Oswald', 'Impact', 'Arial Black', 'sans-serif'],
+      'body': ['"Open Sans"', 'Arial', 'sans-serif'],
+    },
+    
     extend: {},
   },
   plugins: [
