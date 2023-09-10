5fb1b6e26b2888582e5de91b16d1d44be0e9a324
charan sai v <vcharansaiv@gmail.com>
2023-09-10 23:14:43 +0530

    fixed the code in routes at backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,3 @@
-
-<<<<<<< HEAD
 const router = require('express').Router();
 
 const userController = require('../controllers/userController');
@@ -20,23 +18,3 @@ router.get('/all-payments', userAuth, userController.viewAllPayments);
 
 
 module.exports = router;
-
-=======
-const router = require('express')
-const userController = require('../controllers/userController')
-
-
-router.post('/register', userController.userRegister)
-router.post('/login', userController.userLogin)
-router.get('/events', userController.viewAllEvents)
-router.get('/events/:id', userController.viewEventById)
-router.post('/book', userController.bookEvent)
-router.get('/bookings', userController.viewAllBookings)
-router.get('/bookings/:id', userController.viewBookingById)
-router.delete('/bookings/:id', userController.cancelBooking)
-router.post('/payments', userController.makePayment)
-router.get('/payments', userController.viewAllPayments)
-
-
-module.exports = router
->>>>>>> ac24a633c39531ac58c9cfc0cbefb6aeae10dcfd
